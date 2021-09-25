477e5efa1d9e04238c58c7a407eb37a6cd393a7a
abhyankartanvi <abhyankar.tanvi1993@gmail.com>
2021-09-25 15:00:35 +1000

    second container created to display current weather

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,12 @@ const APIKey = "01135a449798815354b5421f38d6f89f";
 let city;
 let citySearch = document.getElementById("city-search");
 let clearHistory = document.querySelector("#clear-history");
+let displayCity = document.querySelector(".display-cityname");
+let displayTemperature = document.querySelector("#temperature");
+let displayHumidity = document.querySelector("#humidity");
+let displayWSpeed = document.querySelector("#wind-speed");
+let displayUVIndex = document.querySelector("#uv-index");
+
 var responseURL =
   "http://api.openweathermap.org/data/2.5/weather?q=" +
   city +
