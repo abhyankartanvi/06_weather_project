a8056ba237d411fe3cc9298eee173c439c26d317
abhyankartanvi <abhyankar.tanvi1993@gmail.com>
2021-09-26 11:37:12 +1000

    reaching out to TA to get help on the 400 error

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,4 @@
-const APIKey = "01135a449798815354b5421f38d6f89f";
-let city;
+let city = "";
 let citySearch = document.getElementById("city-search");
 let clearHistory = document.querySelector("#clear-history");
 let displayCity = document.querySelector(".display-cityname");
@@ -8,8 +7,20 @@ let displayHumidity = document.querySelector("#humidity");
 let displayWSpeed = document.querySelector("#wind-speed");
 let displayUVIndex = document.querySelector("#uv-index");
 
-var responseURL =
-  "http://api.openweathermap.org/data/2.5/weather?q=" +
-  city +
-  "&appid=" +
-  APIKey;
+var APIKey = "01135a449798815354b5421f38d6f89f";
+// creating ajax function to display current weather
+function currentWeather(city) {
+  var queryURL =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    APIKey;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+  });
+}
+
+currentWeather(city);
