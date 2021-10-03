ab44d38eb685469435af1286bfd93e23e2fb7745
abhyankartanvi <abhyankar.tanvi1993@gmail.com>
2021-10-03 19:50:02 +1100

    added date and forcecast

--- assets/script.js
+++ assets/script.js
@@ -24,6 +24,7 @@ function currentWeather(city) {
     method: "GET",
   }).then(function (response) {
     console.log(response);
+
     // updating the icon property
     const weatherIcon = response.weather[0].icon;
     console.log(weatherIcon);
@@ -31,14 +32,20 @@ function currentWeather(city) {
       "http://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
     console.log(iconURL);
     // work on getting the time correct
+    const displayDate = new Date(response.dt * 1000).toDateString();
+    console.log(displayDate);
+
     // add the city name, time and icon
 
-    $(displayCity).html(response.name + "<img src=" + iconURL + ">");
+    $(displayCity).html(
+      response.name + " " + displayDate + "<img src=" + iconURL + ">"
+    );
     console.log(displayCity);
     // adding temp
     $(displayTemperature).text(response.main.temp + " celcius");
     $(displayHumidity).text(" " + response.main.humidity + "%");
     $(displayWSpeed).text(" " + response.wind.speed + " meter/sec ");
+    weatherForecast(city);
 
     // UV index to be added later
   });
@@ -53,6 +60,39 @@ function displayWeather(event) {
   }
 }
 
-$("#search-button").on("click", displayWeather);
+// 5 day forecast
+function weatherForecast(city) {
+  var responseURL =
+    "http://api.openweathermap.org/data/2.5/forecast?q=" +
+    city +
+    "&units=metric" +
+    "&appid=" +
+    APIKey;
+  console.log("this is query URL", responseURL);
+  console.log("this is city", city);
+  $.ajax({
+    url: responseURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    for (i = 0; i < 5; i++) {
+      const iconCode = response.list[i].weather[0].icon;
+      console.log(iconCode);
+      const fIconURL = "http://openweathermap.org/img/wn/" + iconCode + ".png";
+      console.log(fIconURL);
 
-// questions to ask TA - WHY DOES THE ICON API PARAT NOT CONTAIN ? IS IT BECAUSE WE ARE NOT ADDING PRESET PARAM AND SIMPLY ASSIGNING VAR
+      const fTemp = response.list[i].main.temp;
+      const fHum = response.list[i].main.humidity;
+      const fIcon = $("<img>").attr("src", fIconURL);
+      const fDate = new Date(response.list[i].dt * 1000).toDateString();
+      console.log(fDate);
+      $(".futureweatherdate" + i).text(fDate);
+      $(".futureweatherlogo" + i).html(fIcon);
+      $(".futureTemp" + i).text(fTemp + " celcius");
+      $(".futurehumidity" + i).text(fHum + " %");
+    }
+  });
+}
+// weatherForecast(city);
+
+$("#search-button").on("click", displayWeather);
