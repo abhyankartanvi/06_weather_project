518146548b6434a8ddcec19342f30426df82a3f5
abhyankartanvi <abhyankar.tanvi1993@gmail.com>
2021-10-05 12:31:44 +1100

    added local storage

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # 06_weather_project
+
+Description: Weather Dashboard project was my homework assignment for Monash University's coding bootcamp.
+
+This project utlises HTML, CSS and Javascript to render a weather dashboard. I have also utilisted third party APIs namely JQuery and BootStrap. Additionally, I have incorporated server side API open weather API.
+
+Application requires user to input "city" in the search area. Website displays current weather and 5 day forecast.
+
+Challenges: currently working on fixing the 5 day forecast date issue and local storage. Unable to complete due to work commitments.
+
+Screen-shot of the final page:
+
+![image](https://user-images.githubusercontent.com/88257840/135749767-0dc4a96b-a245-4b97-b70d-88203a9d61f2.png)
+
+![image](https://user-images.githubusercontent.com/88257840/135749854-97291935-3859-4faa-97b3-d8b153bab9bf.png)
--- assets/script.js
+++ assets/script.js
@@ -7,6 +7,12 @@ let displayTemperature = document.querySelector("#temperature");
 let displayHumidity = document.querySelector("#humidity");
 let displayWSpeed = document.querySelector("#wind-speed");
 let displayUVIndex = document.querySelector("#uv-index");
+let list = [];
+
+if (localStorage.getItem("displayCity")) {
+  list = localStorage.getItem("displayCity");
+  list = JSON.parse(list);
+}
 
 var APIKey = "01135a449798815354b5421f38d6f89f";
 // creating ajax function to display current weather
@@ -47,6 +53,8 @@ function currentWeather(city) {
     $(displayWSpeed).text(" " + response.wind.speed + " meter/sec ");
     weatherForecast(city);
 
+    makeList();
+
     // UV index to be added later
   });
 }
@@ -57,6 +65,8 @@ function displayWeather(event) {
   if (citySearch.value !== "") {
     city = citySearch.value;
     currentWeather(city);
+    list.push(city);
+    localStorage.setItem("displayCity", JSON.stringify(list));
   }
 }
 
@@ -95,4 +105,26 @@ function weatherForecast(city) {
 }
 // weatherForecast(city);
 
+// create a fn to add cities as list item
+// added as a basic function
+function makeList() {
+  let storageList = JSON.parse(localStorage.getItem("displayCity"));
+  document.querySelector("#city-history").innerHTML = "";
+  for (let i = 0; i < storageList.length; i++) {
+    document.querySelector("#city-history").append(storageList[i] + ", ");
+  }
+}
+
+if (list.length > 0) {
+  makeList();
+}
+
+// work in progress as clear is returning undefined
+function clear(event) {
+  event.preventDefault();
+  document.querySelector("#city-history").innerHTML =
+    localStorage.clear("displayCity");
+}
+
 $("#search-button").on("click", displayWeather);
+$(".clear-history").on("click", clear);
